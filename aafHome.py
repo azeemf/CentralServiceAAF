a2ff65188a4d1540cbce179f19f362e502560d49
Azeem Fidahusein <azeem.fidahusein@aceso.no>
2024-10-26 19:35:04 +0100

    updated path

--- aafHome.py
+++ aafHome.py
@@ -4,7 +4,7 @@ import yaml
 from yaml.loader import SafeLoader
 
 def main():
-    with open('~/SevrviceConfigs.yaml') as file:
+    with open('~/ServiceConfigs/c.yaml') as file:
         config = yaml.load(file, Loader=SafeLoader)
 
     authenticator = stauth.Authenticate(
