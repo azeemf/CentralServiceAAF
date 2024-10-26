1f569e139a2f4224987fd8fa22da8ea7388e4c97
Azeem Fidahusein <azeem.fidahusein@aceso.no>
2024-10-26 22:12:24 +0100

    updated lading test

--- aafLanding.py
+++ aafLanding.py
@@ -1,4 +1,23 @@
 import streamlit as st
 import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+import os
 
-st.write("LoggedIn")
+def main():
+    yaml_path = os.path.expanduser('~/ServiceConfigs/c.yaml') 
+    with open(yaml_path) as file:
+        config = yaml.load(file, Loader=SafeLoader)
+
+    authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+    )
+
+    authenticator.logout()
+
+    st.write("Logged in")
+
+main()
