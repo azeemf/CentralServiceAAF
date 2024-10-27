5dd284b203321e206f1adbd2267d815b8755da2e
Azeem Fidahusein <azeem.fidahusein@aceso.no>
2024-10-27 10:52:38 +0000

    layout testing 2

--- aafFirst.py
+++ aafFirst.py
@@ -4,18 +4,18 @@ import streamlit_authenticator as stauth
 if 'authentication_status' not in st.session_state:
     st.session_state.authentication_status = None
 
-st.set_page_config(
-    page_title="MBA-AAF Services",
-    page_icon="✈️",
-    layout="centered",
-    initial_sidebar_state="auto",
-)
 
 login_page = st.Page("aafHome.py", title="Login - MBA-AAF Services", icon="✈️")
 landing_page = st.Page("aafLanding.py", title="Home - MBA-AAF Services", icon="✈️")
 
 if st.session_state['authentication_status'] is None:
     pg = st.navigation([login_page])
+    st.set_page_config(
+        page_title="MBA-AAF Services",
+        page_icon="✈️",
+        layout="centered",
+        initial_sidebar_state="auto",
+    )
     pg.run()
 
 if st.session_state['authentication_status']:
